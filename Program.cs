8c0dc57dc9d051e89a6ecfa96bc6c5defdf7696b
lucasjotap <leiathemosthot@gmail.com>
2024-01-12 19:49:04 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class RequestClient 
+{
+	private HttpClient _httpClient;
+
+	public HttpClient HttpClient
+	{
+		get
+		{
+			if (_httpClient == null)
+			{
+				_httpClient = new HttpClient();
+			}
+			return _httpClient;
+		}
+	} // 
+
+	static async Task Main(string[] args)
+	{
+		RequestClient requestClient = new RequestClient();
+
+		string apiUrl = "https://dog-api.kinduff.com/api/facts";
+
+		HttpResponseMessage response = await requestClient.HttpClient.GetAsync(apiUrl);
+
+		if (response.IsSuccessStatusCode)
+		{
+			string content = await response.Content.ReadAsStringAsync();
+			Console.WriteLine(content);
+		}
+		else
+		{
+			Console.WriteLine($"Error: {response.StatusCode}");
+		}
+		
+		requestClient.HttpClient.Dispose();
+	}
+}
